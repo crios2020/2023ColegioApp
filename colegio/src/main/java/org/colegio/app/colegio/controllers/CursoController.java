f48d40a1ff591440d1473fca7633e4b4347d3b14
Carlos <intercrios@hotmail.com>
2023-09-23 18:23:59 -0300

    mejoras en h2 jpa

--- colegio/src/main/java/org/colegio/app/colegio/controllers/CursoController.java
+++ colegio/src/main/java/org/colegio/app/colegio/controllers/CursoController.java
@@ -1,30 +1,26 @@
-package org.colegio.app.colegio.test;
+package org.colegio.app.colegio.controllers;
 
 import org.colegio.app.colegio.entities.Curso;
 import org.colegio.app.colegio.enums.Dia;
 import org.colegio.app.colegio.enums.Turno;
 import org.colegio.app.colegio.repositories.CursoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@SpringBootApplication
-public class TestCursoRepository implements CommandLineRunner {
+@Controller
+public class CursoController {
 
     @Autowired
     private CursoRepository cursoRepository;
-    public static void main(String[] args) {
-           SpringApplication.run(TestCursoRepository.class, args);
-    }
- 
-    @Override
-    public void run(String... args) throws Exception {
+
+    @GetMapping("/testCursos")
+    public String getCurso(){
         Curso curso=new Curso("Java","Rios",Dia.LUNES,Turno.MAÑANA);    
         cursoRepository.save(curso);
         System.out.println(curso);
         System.out.println("*********************************************************");
         cursoRepository.findAll().forEach(System.out::println);
+        return "testCursos";
     }
 }
